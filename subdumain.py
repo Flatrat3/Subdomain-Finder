e06d055cab4a43431492a0a77202b35daa2ad3a9
Asim Ehmed <rocco.borocco95@gmail.com>
2025-10-24 13:41:47 +0400

    Project finished

--- subdumain.py
+++ subdumain.py
@@ -1,9 +1,18 @@
 import requests
 
+
+def make_request(url):
+    try:
+        return requests.get(url)
+    except requests.exceptions.ConnectionError:
+        pass
+
 target_input = "google.com"
 
 with open ("subdomains.txt", "r") as subdomain_list:
     for word in subdomain_list:
         word = word.strip()
         url = "http://" + word + "." + target_input
-        response = requests.get(url)
+        response = make_request(url)
+        if response:
+            print(url)
